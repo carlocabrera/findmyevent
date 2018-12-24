f1aecca952849697277051aece3ca4d0c430fb6a
Carlo Cabrera <41243450+carlocabrera@users.noreply.github.com>
2018-12-24 18:02:59 -0500

    fullcalendar

--- index.php
+++ index.php
@@ -1,51 +1,143 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Find My Event</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="css/calendar.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
+
+ 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script>
+<script>
+
+
+ $(document).ready(function() {
+  var date = new Date();
+  var d = date.getDate();
+  var m = date.getMonth();
+  var y = date.getFullYear();
+
+
+  var calendar = $('#calendar').fullCalendar({
+   editable: true,
+   header: {
+    left: 'prev,next today',
+    center: 'title',
+    right: 'month,agendaWeek,agendaDay'
+   },
+
+
+   events: "events.php",
+
+
+   eventRender: function(event, element, view) {
+    if (event.allDay === 'true') {
+     event.allDay = true;
+    } else {
+     event.allDay = false;
+    }
+   },
+   selectable: true,
+   selectHelper: true,
+   select: function(start, end, allDay) {
+   var title = prompt('Event Title:');
+
+
+   if (title) {
+   var start = $.fullCalendar.formatDate(start, "Y-MM-DD HH:mm:ss");
+   var end = $.fullCalendar.formatDate(end, "Y-MM-DD HH:mm:ss");
+   $.ajax({
+	   url: 'add_events.php',
+	   data: 'title='+ title+'&start='+ start +'&end='+ end,
+	   type: "POST",
+	   success: function(json) {
+	   alert('Added Successfully');
+	   }
+   });
+   calendar.fullCalendar('renderEvent',
+   {
+	   title: title,
+	   start: start,
+	   end: end,
+	   allDay: allDay
+   },
+   true
+   );
+   }
+   calendar.fullCalendar('unselect');
+   },
+
+
+   editable: true,
+   eventDrop: function(event, delta) {
+   var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
+   var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
+   $.ajax({
+	   url: 'update_events.php',
+	   data: 'title='+ event.title+'&start='+ start +'&end='+ end +'&id='+ event.id ,
+	   type: "POST",
+	   success: function(json) {
+	    alert("Updated Successfully");
+	   }
+   });
+   },
+   eventClick: function(event) {
+	var decision = confirm("Do you really want to do that?"); 
+	if (decision) {
+	$.ajax({
+		type: "POST",
+		url: "delete_event.php",
+		data: "&id=" + event.id,
+		 success: function(json) {
+			 $('#calendar').fullCalendar('removeEvents', event.id);
+			  alert("Updated Successfully");}
+	});
+	}
+  	},
+   eventResize: function(event) {
+	   var start = $.fullCalendar.formatDate(event.start, "yyyy-MM-dd HH:mm:ss");
+	   var end = $.fullCalendar.formatDate(event.end, "yyyy-MM-dd HH:mm:ss");
+	   $.ajax({
+	    url: 'update_events.php',
+	    data: 'title='+ event.title+'&start='+ start +'&end='+ end +'&id='+ event.id ,
+	    type: "POST",
+	    success: function(json) {
+	     alert("Updated Successfully");
+	    }
+	   });
+	}
+   
+  });
+  
+ });
+
+
+</script>
+<style>
+ body {
+  margin-top: 40px;
+  text-align: center;
+  font-size: 14px;
+  font-family: "Lucida Grande",Helvetica,Arial,Verdana,sans-serif;
+  }
+ #calendar {
+  width: 650px;
+  margin: 0 auto;
+  }
+</style>
 </head>
+
+
 <body>
 <div class="container">
-<div class="page-header">
-<div class="pull-right form-inline">
-<div class="btn-group">
-<button class="btn btn-primary" data-calendar-nav="prev"><< Prev</button>
-<button class="btn btn-default" data-calendar-nav="today">Today</button>
-<button class="btn btn-primary" data-calendar-nav="next">Next >></button>
+  <div class="jumbotron">
+    <h1>Find My Event</h1>  
+  </div>
 </div>
-<div class="btn-group">
-<button class="btn btn-warning" data-calendar-view="year">Year</button>
-<button class="btn btn-warning active" data-calendar-view="month">Month</button>
-<button class="btn btn-warning" data-calendar-view="week">Week</button>
-<button class="btn btn-warning" data-calendar-view="day">Day</button>
-</div>
-</div>
-<h3></h3>
-<h1>Find My Event</h1>
-</div>
-<div class="row">
-<div class="col-md-9">
-<div id="showEventCalendar"></div>
-</div>
-<div class="col-md-3">
-<h4>Events List</h4>
-<ul id="eventlist" class="nav nav-list"></ul>
-</div>
-</div>
-</div>    
-
-
-
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js"></script>
-<script type="text/javascript" src="js/calendar.js"></script>
-<script type="text/javascript" src="js/events.js"></script>
+ <br/>
+ <div id='calendar'></div>
 </body>
+
+
 </html>
