e4c8ebd6b5ceb8e53d9b1946dd00095d93e53288
Carlo Cabrera <41243450+carlocabrera@users.noreply.github.com>
2019-01-07 22:13:42 -0500

    3.0

--- models/events.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-    var Event = sequelize.define("Events", {
-      title: DataTypes.STRING,
-      start: DataTypes.DATE,
-      end: DataTypes.DATE,
-    });
-    return Event;
-  };
