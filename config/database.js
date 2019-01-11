4f622523fa0863d085cc66b8a59167fd17c9a6a8
Carlo Cabrera <41243450+carlocabrera@users.noreply.github.com>
2019-01-11 01:38:32 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,9 +1,22 @@
 const Sequelize = require('sequelize');
 
-module.exports =  new Sequelize('findmyevent', 'root', 'root', {
-  host: 'localhost',
-  dialect: 'mysql',
-  operatorsAliases: false,
+// module.exports =  new Sequelize('findmyevent', 'root', 'root', {
+//   host: 'localhost',
+//   dialect: 'mysql',
+//   operatorsAliases: false,
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection(
+	{
+		host: 'localhost',
+		port: 3306,
+		user: 'root',
+		password: 'root',
+		database: 'findmyevent'
+	});
+}
 
   pool: {
     max: 5,
